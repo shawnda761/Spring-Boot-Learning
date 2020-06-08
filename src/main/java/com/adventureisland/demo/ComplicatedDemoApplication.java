4aeac7b05631f7638364d8ab336474210a613a7e
ShawnDa <shawnda761@gmail.com>
2020-06-09 09:59:59 +1200

    Project initialisation.

---
+++ src/main/java/com/adventureisland/demo/ComplicatedDemoApplication.java
@@ -0,0 +1,13 @@
+package com.adventureisland.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplicatedDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplicatedDemoApplication.class, args);
+	}
+
+}
